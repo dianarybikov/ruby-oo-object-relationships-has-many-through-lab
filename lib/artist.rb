8fcc5e33f0870a7ca0c8b6f332f8b25cad7f9d40
Diana Rybikov <diana.ryb214@gmail.com>
2020-12-31 14:48:04 -0800

    commit tree 2e8befe8d5f807b1b0055dea3f7c3888847b1d44

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,32 @@
+class Artist 
+attr_accessor :name, :genre
+    @@all = []
 
+    def initialize name
+    @name = name
+    @@all << self
+    end
+
+    def self.all
+    @@all
+    end
+
+    def songs
+        Song.all.select do |song|
+            song.artist == self
+    end
+end
+
+    def genre
+        Artist.songs.collect do |artist|
+            artist.genre 
+        end
+    end
+
+
+
+
+
+
+
+end
--- lib/genre.rb
+++ lib/genre.rb
@@ -1,1 +1,28 @@
+class Genre
+attr_accessor :name
+    @@all = []
 
+    def initialize name
+        @name = name
+        @@all << self   
+    end
+
+    def self.all
+        @@all
+    end
+
+    def songs
+        Songs.all.select do |song|
+            song.genre == self
+        end
+    end
+
+    def artists
+        Genre.songs.select do |song|
+            artist.song
+        end
+    end
+
+
+
+end
